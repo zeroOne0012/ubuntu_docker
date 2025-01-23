d171cf4e0cd3bdde150640fa5fdd890f291cb4eb
zeroOne0012 <012012012il@gmail.com>
2025-01-23 16:34:12 +0900

    https

--- Eggtec-backend/module/postgres.js
+++ Eggtec-backend/module/postgres.js
@@ -1,11 +1,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "handalab",
-  password: "handalab",
-  host: "localhost",
-  database: "eggtec",
-  port: 5432,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DB,
+  port: process.env.POSTGRES_PORT,
   idleTimeoutMillis: 3000,
   connectionTimeoutMillis: 30000,
   max: 10,
